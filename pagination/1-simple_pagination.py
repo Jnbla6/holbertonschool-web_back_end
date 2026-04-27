d083ec01c70a9eb4b146e34e1f9d4195cc24a2a1
JNBLA6 <badrsha321@gmail.com>
2026-04-27 17:34:48 +0300

    solve

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -9,9 +9,7 @@ from typing import List, Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters.
+    Return a tuple of size two containing wedewfew
     """
     return ((page - 1) * page_size, page * page_size)
 
